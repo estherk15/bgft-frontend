d2bba4ab76ce15bd2fdcb19457e76a044aeda10e
github username <github email address>
2019-02-07 16:57:54 -0500

    Started on SightingsContainer

--- src/components/SightingContainer.js
+++ src/components/SightingContainer.js
@@ -3,15 +3,28 @@ import Sighting from './Sighting'
 
 class SightingsContainer extends React.Component {
 
+  state = {
+
+  }
+
+
+
   render() {
     return (
-
       <div>
-        {this.props.sightings.map(sighting =>
-          <Sighting sighting={sighting}/>
-        )
-        }
+        <div>
+          <form onSubmit=>
+            <input type="text" placeholder="Enter a Location" />
+            <input type="text" placeholder="Enter a Description" />
+            <input type="text" placeholder="Photo Url" />
+            <input type="number" placeholder="User ID" />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+
+        {this.props.sightings.map(sighting => <Sighting key={sighting.id} sighting={sighting}/>)}
       </div>
+
     )
   }
 }
