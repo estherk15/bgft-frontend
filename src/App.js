e72df783273db43cff3ce4a6aa19dfc36d19dacf
github username <github email address>
2019-02-08 10:59:11 -0500

    Sightings handleClick handleSubmit

--- src/App.js
+++ src/App.js
@@ -13,23 +13,28 @@ class App extends Component {
     sightings: [],
   }
 
+  addSighting = (sighting) => {
+    this.setState({sightings: [sighting, ...this.state.sightings]})
+  }
+
   componentDidMount() {
     // fetch from local API bfgt-backend
     fetch('http://localhost:3000//api/v1/users')
       .then(response => response.json())
-      .then(userData => this.setState({ userData }))
+      .then(userData => this.setState({ userData: userData }))
 
     fetch('http://localhost:3000//api/v1/sightings')
       .then(response => response.json())
-      .then(sightings => this.setState({ sightings }));
+      .then(sightings => this.setState({ sightings: sightings }));
   }
 
   render() {
+    // console.log(this.state)
     return (
       <div className="App">
         <NavBar />
         <h1>Big Foot Finder</h1>
-        <SightingContainer sightings={this.state.sightings}/>
+        <SightingContainer sightings={this.state.sightings} handleSubmit={this.handleSubmit} addSighting={this.addSighting}/>
       </div>
     );
   }
--- src/components/SightingContainer.js
+++ src/components/SightingContainer.js
@@ -4,21 +4,51 @@ import Sighting from './Sighting'
 class SightingsContainer extends React.Component {
 
   state = {
+    location: '',
+    description: '',
+    photo: '',
+    userId: '',
+  }
 
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+    fetch('http://localhost:3000//api/v1/sightings', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify({
+        location: this.state.location,
+        description: this.state.description,
+        photo: this.state.photo,
+        user_id: this.state.userId
+      })
+    })
+    .then(r => r.json())
+    .then(sighting => this.props.addSighting(sighting))
   }
 
 
-
   render() {
+    // console.log(this.state);
+
     return (
       <div>
         <div>
-          <form onSubmit={'add some function'}>
-            <input type="text" placeholder="Enter a Location" />
-            <input type="text" placeholder="Enter a Description" />
-            <input type="text" placeholder="Photo Url" />
-            <input type="number" placeholder="User ID" />
-            <input type="submit" value="Submit" />
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" name="location" placeholder="Enter a Location" onChange={this.handleChange}/>
+            <input type="text" name="description" placeholder="Enter a Description" onChange={this.handleChange}/>
+            <input type="text" name="photo" placeholder="Photo Url" onChange={this.handleChange}/>
+            <input type="number" name="userId" placeholder="User ID" onChange={this.handleChange}/>
+            <input type="submit" value="Submit"/>
           </form>
         </div>
 
